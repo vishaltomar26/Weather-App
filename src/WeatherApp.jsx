2f9d2c219af90b9e8d409d626d328f12da68173c
Vishal Tomar <165481683+vishaltomar26@users.noreply.github.com>
2025-08-05 14:05:27 +0530

    Update WeatherApp.jsx

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -4,7 +4,7 @@ import InfoBox from './InfoBox';
 export default function WeatherApp() {
     return (
         <div >
-            <h2>Weather App By Delta</h2>
+            <h2>Weather App By </h2>
             <SearchBox />
             <InfoBox/>
         </div>
